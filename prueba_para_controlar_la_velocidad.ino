618058d58014c375d7dec60f000ac1fede0f242d
Kevni-Rodrigo-Wawryniuk <126299266+Kevni-Rodrigo-Wawryniuk@users.noreply.github.com>
2023-06-12 11:00:29 -0300

    esto controla la velocidad con la aplicacion

---
+++ prueba_para_controlar_la_velocidad.ino
@@ -0,0 +1,200 @@
+double num;
+double num1;
+double num2;
+boolean b_autonomo;
+
+void setup()
+{
+    pinMode(9, OUTPUT);
+  pinMode(10, OUTPUT);
+  pinMode(11, OUTPUT);
+  pinMode(6, OUTPUT);
+  pinMode(7, OUTPUT);
+  pinMode(5, OUTPUT);
+
+  Serial.begin(9600);
+  Serial.flush();
+  while(Serial.available()>0)Serial.read();
+
+  pinMode(A0, INPUT);
+  pinMode(A1, INPUT);
+
+  num1 = 255;
+  b_autonomo = false;
+
+}
+
+
+void loop()
+{
+
+    num = Serial.read();
+    if ((num == 87)) {
+      b_autonomo = true;
+    }
+
+    if ((num == 119)) {
+      b_autonomo = false;
+    }
+
+    if (((b_autonomo == true) && (num > -1))) {
+      if (((analogRead(A0) > 600) && (analogRead(A1) > 600))) {
+        digitalWrite(9,HIGH);
+        digitalWrite(10,LOW);
+        analogWrite(11,num1);
+        digitalWrite(6,HIGH);
+        digitalWrite(7,LOW);
+        analogWrite(5,num1);
+        Serial.println(String("adelante"));
+      }
+
+      if (((analogRead(A0) < 100) && (analogRead(A1) > 600))) {
+        digitalWrite(9,HIGH);
+        digitalWrite(10,LOW);
+        analogWrite(11,num1);
+        digitalWrite(6,LOW);
+        digitalWrite(7,HIGH);
+        analogWrite(5,num1);
+        Serial.println(String("derecha"));
+      }
+
+      if (((analogRead(A0) > 600) && (analogRead(A1) < 100))) {
+        digitalWrite(9,LOW);
+        digitalWrite(10,HIGH);
+        analogWrite(11,num1);
+        digitalWrite(6,HIGH);
+        digitalWrite(7,LOW);
+        analogWrite(5,num1);
+        Serial.println(String("izquierda"));
+      }
+
+      if (((analogRead(A0) < 100) && (analogRead(A1) < 100))) {
+        digitalWrite(9,LOW);
+        digitalWrite(10,LOW);
+        analogWrite(11,num1);
+        digitalWrite(6,LOW);
+        digitalWrite(7,LOW);
+        analogWrite(5,num1);
+        Serial.println(String("Detener"));
+      }
+
+      if ((num == 113)) {
+        num1 = 255;
+        Serial.println(String("maxima velocidad"));
+      }
+
+      if (((num >= 48) && (num <= 57))) {
+        num2 = (num - 48);
+        num1 = map(num2, 0,9,60,225);
+        Serial.println(String("velocidad")+String(num2));
+      }
+
+    }
+    else {
+      if ((num > -1)) {
+        if ((num == 70)) {
+          digitalWrite(9,HIGH);
+          digitalWrite(10,LOW);
+          analogWrite(11,num1);
+          digitalWrite(6,HIGH);
+          digitalWrite(7,LOW);
+          analogWrite(5,num1);
+          Serial.println(String("adelante"));
+        }
+
+        if ((num == 66)) {
+          digitalWrite(9,LOW);
+          digitalWrite(10,HIGH);
+          analogWrite(11,num1);
+          digitalWrite(6,LOW);
+          digitalWrite(7,HIGH);
+          analogWrite(5,num1);
+          Serial.println(String("atras"));
+        }
+
+        if ((num == 82)) {
+          digitalWrite(9,HIGH);
+          digitalWrite(10,LOW);
+          analogWrite(11,num1);
+          digitalWrite(6,LOW);
+          digitalWrite(7,HIGH);
+          analogWrite(5,num1);
+          Serial.println(String("derecha"));
+        }
+
+        if ((num == 76)) {
+          digitalWrite(9,LOW);
+          digitalWrite(10,HIGH);
+          analogWrite(11,num1);
+          digitalWrite(6,HIGH);
+          digitalWrite(7,LOW);
+          analogWrite(5,num1);
+          Serial.println(String("izquierda"));
+        }
+
+        if ((num == 83)) {
+          digitalWrite(9,LOW);
+          digitalWrite(10,LOW);
+          analogWrite(11,num1);
+          digitalWrite(6,LOW);
+          digitalWrite(7,LOW);
+          analogWrite(5,num1);
+          Serial.println(String("parar"));
+        }
+
+        if ((num == 71)) {
+          digitalWrite(9,LOW);
+          digitalWrite(10,LOW);
+          analogWrite(11,num1);
+          digitalWrite(6,HIGH);
+          digitalWrite(7,LOW);
+          analogWrite(5,num1);
+          Serial.println(String("adelante izquierda"));
+        }
+
+        if ((num == 73)) {
+          digitalWrite(9,HIGH);
+          digitalWrite(10,LOW);
+          analogWrite(11,num1);
+          digitalWrite(6,LOW);
+          digitalWrite(7,LOW);
+          analogWrite(5,num1);
+          Serial.println(String("adelante derecha"));
+        }
+
+        if ((num == 72)) {
+          digitalWrite(9,LOW);
+          digitalWrite(10,HIGH);
+          analogWrite(11,num1);
+          digitalWrite(6,LOW);
+          digitalWrite(7,LOW);
+          analogWrite(5,num1);
+          Serial.println(String("atras derecha"));
+        }
+
+        if ((num == 74)) {
+          digitalWrite(9,LOW);
+          digitalWrite(10,LOW);
+          analogWrite(11,num1);
+          digitalWrite(6,LOW);
+          digitalWrite(7,HIGH);
+          analogWrite(5,num1);
+          Serial.println(String("atras izquierda"));
+        }
+
+        if ((num == 113)) {
+          num1 = 255;
+          Serial.println(String("maxima velocidad"));
+        }
+
+        if (((num >= 48) && (num <= 57))) {
+          num2 = (num - 48);
+          num1 = map(num2, 0,9,60,225);
+          Serial.println(String("velocidad")+String(num2));
+        }
+
+      }
+
+    }
+
+}
